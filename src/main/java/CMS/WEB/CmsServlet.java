d71c26ee204533c5b4402e1d26d858634fd803d9
franck <franck.hubschwerle@uha.fr>
2019-04-05 14:32:52 +0200

    fix servlet

--- src/main/java/CMS/WEB/CmsServlet.java
+++ src/main/java/CMS/WEB/CmsServlet.java
@@ -1,4 +1,4 @@
-package WEB;
+package CMS.WEB;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class CmsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.getWriter().println("Index");
     }
 }
