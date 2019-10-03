3cc5524bb34ffd2e09f11fb84cae188ca01f8d16
franck <franck.hubschwerle@uha.fr>
2019-10-03 23:22:47 +0200

    ajout de la vue via jsp

--- src/main/java/CMS/WEB/CPageServlet.java
+++ src/main/java/CMS/WEB/CPageServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,8 +22,12 @@ public class CPageServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        //resp.setContentType("text/html");
+        //resp.setCharacterEncoding("UTF-8");
+        this.getServletContext().getRequestDispatcher( "/WEB-INF/vue.jsp" ).forward( req, resp );
         displayForm(resp);
+
+
     }
 @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
