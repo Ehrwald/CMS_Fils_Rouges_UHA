d1b227252184b5e2a8c06dab294e1c9f1b33d162
franck <franck.hubschwerle@uha.fr>
2019-10-08 17:12:32 +0200

    Migration des teste général dans CParagrapheTest vers CpageTest et ajout de la méthode POST à tester

--- test/CMS/CParagraphTest.java
+++ test/CMS/CParagraphTest.java
@@ -32,31 +32,5 @@ class CParagraphTest {
             e.printStackTrace();
         }
     }*/
-    @Test
-   void testSendGet() throws IOException {
-       String url = "http://localhost:8080/cpage";
 
-       URL obj = new URL(url);
-       HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-       int responseCode = con.getResponseCode();
-       System.out.println("\nSending 'GET' request to URL : " + url);
-       System.out.println("Response Code : " + responseCode);
-
-       BufferedReader in = new BufferedReader(
-               new InputStreamReader(con.getInputStream()));
-       String inputLine;
-       StringBuffer response = new StringBuffer();
-
-       while ((inputLine = in.readLine()) != null) {
-           response.append(inputLine);
-       }
-       in.close();
-
-       System.out.println(response.toString());
-
-        assertEquals(200, responseCode);
-        assertEquals("http://localhost:8080/cpage", url);
-
-   }
 }
